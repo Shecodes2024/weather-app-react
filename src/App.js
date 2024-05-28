cde2aee437d1cc35f42ca890ae4690ea5a58d406
Loise Musungu <mwongelimusungu@gmail.com>
2024-05-28 18:39:06 +0300

    added the footer with github and netlify links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://weather-app-react-a.netlify.app/" target="_blank">
+          {" "}
+          Loise Musungu
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/Shecodes2024/weather-app-react"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
