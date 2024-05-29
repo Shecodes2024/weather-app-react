b127828384970e8fe7d4eccff806d5210cfeb17f
Loise Musungu <mwongelimusungu@gmail.com>
2024-05-29 08:37:26 +0300

    Installed bootsrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a href="https://weather-app-react-a.netlify.app/" target="_blank">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
           {" "}
-          Loise Musungu
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/Shecodes2024/weather-app-react"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+          This project was coded by{" "}
+          <a href="https://weather-app-react-a.netlify.app/" target="_blank">
+            {" "}
+            Loise Musungu
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/Shecodes2024/weather-app-react"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
